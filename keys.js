26925c523f39edbe413be744676911f812c88feb
x <x@xs-MacBook-Pro.local>
2017-10-09 23:53:13 -0500

    third commit

--- keys.js
+++ keys.js
@@ -1,10 +1,9 @@
 console.log('this is loaded');
 
-var twitterKeys = {
+exports.twitterKeys = {
   consumer_key: 'uxl6GvoQHdqrEraxNsvQT90nk',
   consumer_secret: 'X9DzCjVkZ2YIyDPOffr35aVZxaPwKMLePxS0Vx0seTmsbSpQLR',
   access_token_key: '917189046438383616-wFhdoWGTIJQzgvFkh2Q4Nn8pRYk3drC',
   access_token_secret: 'V6VZgPsa2AQaLIXgTGuI365u6DDswdeeblUFsLHWwtz6m',
 }
 
-module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -4,12 +4,20 @@ var request = require('request');
 
 var Twitter = require('twitter');
  
+
 var client = new Twitter(keys.twitterKeys);
  
-var params = {screen_name: 'dooosetrain'};
+var params = {screen_name: "dooosetrain"};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+    for (var i = 0; i < tweets.length; i++) {
+    	console.log(tweets[i].created_at);
+    	console.log(' ');
+    	console.log(tweets[i].text)
+    }
   }
 });
+
+
 
