6b930305da374122760ea6c5b7cff3b0d1a3f48c
x <x@xs-MacBook-Pro.local>
2017-10-10 22:32:18 -0500

    final commit

--- keys.js
+++ keys.js
@@ -1,9 +1,13 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+exports.twitterKeys = { 
   consumer_key: 'uxl6GvoQHdqrEraxNsvQT90nk',
   consumer_secret: 'X9DzCjVkZ2YIyDPOffr35aVZxaPwKMLePxS0Vx0seTmsbSpQLR',
   access_token_key: '917189046438383616-wFhdoWGTIJQzgvFkh2Q4Nn8pRYk3drC',
   access_token_secret: 'V6VZgPsa2AQaLIXgTGuI365u6DDswdeeblUFsLHWwtz6m',
-}
+},//"exports." turned out to be the correct syntax in the case against module.exports = twitterKeyes... Don't ask me why - it was crazy 
 
+exports.spotifyKeys = {
+	id: '1e978b15193a419eadcd88157dbadbe2',
+  	secret: 'c27a4e4c763844ea8397fbb03c1e3645',
+}
--- liri.js
+++ liri.js
@@ -1,23 +1,99 @@
 var keys = require("./keys.js");
 
+var Twitter = require('twitter');
+
+var spotify = require('spotify');
+
+var Spotify = require('node-spotify-api');
+
 var request = require('request');
 
-var Twitter = require('twitter');
+
+
+//-------Twitter-------//
+
+var getMyTweets = function() {
+	var client = new Twitter(keys.twitterKeys); 
+	var params = {screen_name: "dooosetrain"};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    for (var i = 0; i < tweets.length; i++) {
+	    	console.log(tweets[i].created_at);
+	    	console.log(' ');
+	    	console.log(tweets[i].text)
+	    }
+	  }
+	}); 
+}
+ 
+//---------Spotify---------//
+var getArtistNames = function(artist) {
+	return artist.name;
+}
+
+var getSpotify = function(songName) {
+	var spotify = new Spotify(keys.spotifyKeys);
+
+	spotify.search({ type: 'track', query: songName  }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+
+	  var songs = data.tracks.items;
+	  for (var i = 0; i < songs.length; i++) {
+	  	console.log(i);
+	  	console.log('artist(s): ' + songs[i].artists.map(
+	  		getArtistNames));
+	  	console.log('song name: ' + songs[i].name);
+	  	console.log('preview song: ' + songs[i].preview_url);
+	  	console.log('album: ' + songs[i].album.name);
+	  	console.log('--------------------------------------');
+	  }
+	});	
+}
+
+
+
+
+//----------Request--------//
+
+// Note: Not Working - A constant theme on this project was not getting my 
+//Console.Logs to pull what I initially want without a thousand trial and errors
+//Creating the API KEY seems to be the right answer and I think my syntax is correct
+//but it still doesnt show.
+//the remaining PSeudo Code would be to turn this request into a GetMovie function that would be called by movie-this.
+
+request('http://www.omdbapi.com/?apikey=40e9cece&?t=pulp+fiction', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+//-------Liri FUnction------//
+
+var pick = function(caseData, functionData) {
+	switch(caseData) {
+		case 'my-tweets' :
+			getMyTweets();
+			break;
+		case 'spotify-this-song' :
+			getSpotify(functionData);
+			break;
+		case 'movie-this' :
+			getMovie();
+
+		default:
+		console.log('Liri dont know that');
+	}
+}
+
+var runThis = function(argOne, argTwo) {
+	pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]); // process.argv2 will take the "my-tweets, getspotify" commands and argv3 will be the next most specific search
  
 
-var client = new Twitter(keys.twitterKeys);
- 
-var params = {screen_name: "dooosetrain"};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-    for (var i = 0; i < tweets.length; i++) {
-    	console.log(tweets[i].created_at);
-    	console.log(' ');
-    	console.log(tweets[i].text)
-    }
-  }
-});
 
 
 
